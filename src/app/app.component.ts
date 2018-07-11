5c27fdcfdba904e1178f29cba0bbda3aaa8fb73f
Alexdev <afanasiev.uae@gmail.com>
2018-07-11 23:20:11 +0400

    smalltest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { jsPlumb } from 'jsplumb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  jsPlumbInstance;
+
+  ngAfterViewInit() {
+    this.jsPlumbInstance = jsPlumb.getInstance();
+    const endpoint1 = this.jsPlumbInstance.addEndpoint('page1'),
+        endpoint2 = this.jsPlumbInstance.addEndpoint('page2');
+        this.jsPlumbInstance.connect({ source: endpoint1, target: endpoint2 });
+        this.jsPlumbInstance.draggable('page1');
+        this.jsPlumbInstance.draggable('page2');
+  }
 }
